834e1cfb01ea065f8b4287bd1607fc5268d255b0
Kotsasmin <68585878+Kotsasmin@users.noreply.github.com>
2023-08-27 15:03:38 +0300

    Update scroll.js
    Made it to wait for DOM to start

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,12 +1,14 @@
-const scrollLinks = document.querySelectorAll('.scroll');
-scrollLinks.forEach(function(link) {
-  link.addEventListener('click', function(event) {
-    event.preventDefault();
-    const target = document.querySelector(link.getAttribute('href'));
-              const offsetTop = target.offsetTop;
-              window.scrollTo({
-                top: offsetTop,
-                behavior: 'smooth'
-              });
-            });
+document.addEventListener("DOMContentLoaded", function() {
+  const scrollLinks = document.querySelectorAll('.scroll');
+  scrollLinks.forEach(function(link) {
+      link.addEventListener('click', function(event) {
+          event.preventDefault();
+          const target = document.querySelector(link.getAttribute('href'));
+          const offsetTop = target.offsetTop;
+          window.scrollTo({
+              top: offsetTop,
+              behavior: 'smooth'
           });
+      });
+  });
+});

cdbefb403e4770e0b4861e637e533b228843d74a
Kotsasmin <68585878+Kotsasmin@users.noreply.github.com>
2023-08-27 15:05:06 +0300

    Update clipboard.js
    Fixed not displaying the message

--- assets/js/clipboard.js
+++ assets/js/clipboard.js
@@ -1,14 +1,21 @@
 function copyText() {
-                navigator.clipboard.writeText
-            ("play.1234.com");
-                }
-            $(document).ready(function() {
-            $('.main1_ipcopier').click(function() {
-                var popup = $('<div>', { class: 'main1_popup' })
-                .append($('<h3 class="copy_confirm">').text('Copied IP to clipboard'))
-                $(this).after(popup);
-                setTimeout(function() {
-                popup.remove();
-                }, 1000);
-           });
+    const textToCopy = "hoodliferp.xyz";
+    navigator.clipboard.writeText(textToCopy)
+        .then(function() {
+            // Svsta
+            $('.main1_ipcopier').after('<div class="main1_popup"><h3 class="copy_confirm">Copied IP to clipboard</h3></div>');
+            setTimeout(function() {
+                $('.main1_popup').remove();
+            }, 1000);
+        })
+        .catch(function(error) {
+            // Lathos
+            console.error('Failed to copy: ', error);
+        });
+}
+
+$(document).ready(function() {
+    $('.main1_ipcopier').click(function() {
+        copyText(); // Na treksei to function gt den ginetai callback apla canontas mention to js
+    });
 });
